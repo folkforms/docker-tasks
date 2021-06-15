a0b42cb615ee4bc93eb0677abd3ddbf7c1a96d57
folkforms <alan.bergin@gmail.com>
2021-06-15 23:29:28 +0100

    Improved tests

--- docker-tasks.test.js
+++ docker-tasks.test.js
@@ -20,7 +20,7 @@ test("when we call 'build' it executes the correct command", () => {
 test("when we call 'run' it executes the correct command", () => {
   const exitCode = dockerTasks(dummyShellJs, ["run"]);
   expect(exitCode).toEqual(0);
-  expect(dummyShellJs.commands).toContain("exec('docker run --name foo foo:latest')");
+  expect(dummyShellJs.commands).toContain("exec('docker run -p 3000:3000 --name foo foo:latest')");
 });
 
 test("when we call 'debug' it executes the correct command", () => {
@@ -32,6 +32,7 @@ test("when we call 'debug' it executes the correct command", () => {
 test("when we call 'clear' it executes the correct commands", () => {
   const exitCode = dockerTasks(dummyShellJs, ["clear"]);
   expect(exitCode).toEqual(0);
+  expect(dummyShellJs.commands.length).toEqual(4); // 2 lines of info text plus 2 commands
   expect(dummyShellJs.commands).toContain("exec('docker stop foo')");
   expect(dummyShellJs.commands).toContain("exec('docker rm foo')");
 });
@@ -39,6 +40,7 @@ test("when we call 'clear' it executes the correct commands", () => {
 test("when we call 'release 1.0.0' it executes the correct commands", () => {
   const exitCode = dockerTasks(dummyShellJs, ["release", "1.0.0"]);
   expect(exitCode).toEqual(0);
+  expect(dummyShellJs.commands.length).toEqual(7); // 2 lines of info text plus 5 commands
   expect(dummyShellJs.commands).toContain("exec('docker image tag foo:latest foo:1.0.0')");
   expect(dummyShellJs.commands).toContain("exec('docker image tag foo:latest docker.io/folkforms/foo:1.0.0')");
   expect(dummyShellJs.commands).toContain("exec('docker image tag foo:latest docker.io/folkforms/foo:latest')");
@@ -47,7 +49,7 @@ test("when we call 'release 1.0.0' it executes the correct commands", () => {
 });
 
 test("when we call 'release latest' it executes the correct commands", () => {
-  const exitCode = dockerTasks(dummyShellJs, ["release", "1.0.0"]);
+  const exitCode = dockerTasks(dummyShellJs, ["release", "latest"]);
   expect(exitCode).toEqual(0);
   expect(dummyShellJs.commands).toContain("exec('docker image tag foo:latest docker.io/folkforms/foo:latest')");
   expect(dummyShellJs.commands).toContain("exec('docker image push docker.io/folkforms/foo:latest')");
