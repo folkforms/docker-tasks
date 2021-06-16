a829348d82023ef87ceffc878b1fccdf232effc6
folkforms <alan.bergin@gmail.com>
2021-06-16 01:02:32 +0100

    Improved tests

--- docker-tasks.test.js
+++ docker-tasks.test.js
@@ -2,67 +2,68 @@ const dockerTasks = require("./docker-tasks");
 const dummyShellJs = require("./dummyShellJs");
 
 beforeEach(() => {
-  dummyShellJs.commands = [];
+  dummyShellJs._clear();
 });
 
 test("when we call 'genconfig' it executes the correct command", () => {
   const exitCode = dockerTasks(dummyShellJs, ["genconfig"]);
   expect(exitCode).toEqual(0);
-  expect(dummyShellJs.commands).toContain("cp('./node_modules/docker-tasks/.docker-tasks-default-config.yml', './.docker-tasks.yml')");
+  expect(dummyShellJs.cpList).toContain("cp ./node_modules/docker-tasks/.docker-tasks-default-config.yml ./.docker-tasks.yml");
 });
 
 test("when we call 'build' it executes the correct command", () => {
   const exitCode = dockerTasks(dummyShellJs, ["build"]);
   expect(exitCode).toEqual(0);
-  expect(dummyShellJs.commands).toContain("exec('docker build --tag foo:latest .')");
+  expect(dummyShellJs.execList).toContain("docker build --tag foo:latest .");
 });
 
 test("when we call 'run' it executes the correct command", () => {
   const exitCode = dockerTasks(dummyShellJs, ["run"]);
   expect(exitCode).toEqual(0);
-  expect(dummyShellJs.commands).toContain("exec('docker run -p 3000:3000 --name foo foo:latest')");
+  expect(dummyShellJs.execList).toContain("docker run -p 3000:3000 --name foo foo:latest");
 });
 
 test("when we call 'debug' it executes the correct command", () => {
   const exitCode = dockerTasks(dummyShellJs, ["debug"]);
   expect(exitCode).toEqual(0);
-  expect(dummyShellJs.commands).toContain("echo('    docker run  --tty --interactive --entrypoint bash foo:latest')");
+  expect(dummyShellJs.echoList).toContain("    docker run  --tty --interactive --entrypoint bash foo:latest");
 });
 
 test("when we call 'clear' it executes the correct commands", () => {
   const exitCode = dockerTasks(dummyShellJs, ["clear"]);
   expect(exitCode).toEqual(0);
-  expect(dummyShellJs.commands.length).toEqual(4); // 2 lines of info text plus 2 commands
-  expect(dummyShellJs.commands).toContain("exec('docker stop foo')");
-  expect(dummyShellJs.commands).toContain("exec('docker rm foo')");
+  expect(dummyShellJs.execList.length).toEqual(2);
+  expect(dummyShellJs.execList).toContain("docker stop foo");
+  expect(dummyShellJs.execList).toContain("docker rm foo");
 });
 
 test("when we call 'release 1.0.0' it executes the correct commands", () => {
   const exitCode = dockerTasks(dummyShellJs, ["release", "1.0.0"]);
   expect(exitCode).toEqual(0);
-  expect(dummyShellJs.commands.length).toEqual(7); // 2 lines of info text plus 5 commands
-  expect(dummyShellJs.commands).toContain("exec('docker image tag foo:latest foo:1.0.0')");
-  expect(dummyShellJs.commands).toContain("exec('docker image tag foo:latest docker.io/folkforms/foo:1.0.0')");
-  expect(dummyShellJs.commands).toContain("exec('docker image tag foo:latest docker.io/folkforms/foo:latest')");
-  expect(dummyShellJs.commands).toContain("exec('docker image push docker.io/folkforms/foo:latest')");
-  expect(dummyShellJs.commands).toContain("exec('docker image push docker.io/folkforms/foo:1.0.0')");
+  expect(dummyShellJs.execList.length).toEqual(5);
+  expect(dummyShellJs.execList).toContain("docker image tag foo:latest foo:1.0.0");
+  expect(dummyShellJs.execList).toContain("docker image tag foo:latest docker.io/folkforms/foo:1.0.0");
+  expect(dummyShellJs.execList).toContain("docker image tag foo:latest docker.io/folkforms/foo:latest");
+  expect(dummyShellJs.execList).toContain("docker image push docker.io/folkforms/foo:latest");
+  expect(dummyShellJs.execList).toContain("docker image push docker.io/folkforms/foo:1.0.0");
 });
 
 test("when we call 'release latest' it executes the correct commands", () => {
   const exitCode = dockerTasks(dummyShellJs, ["release", "latest"]);
   expect(exitCode).toEqual(0);
-  expect(dummyShellJs.commands).toContain("exec('docker image tag foo:latest docker.io/folkforms/foo:latest')");
-  expect(dummyShellJs.commands).toContain("exec('docker image push docker.io/folkforms/foo:latest')");
+  expect(dummyShellJs.execList.length).toEqual(2);
+  expect(dummyShellJs.execList).toContain("docker image tag foo:latest docker.io/folkforms/foo:latest");
+  expect(dummyShellJs.execList).toContain("docker image push docker.io/folkforms/foo:latest");
 });
 
 test("when we pass an unknown command we get an error message and a non-zero exit code", () => {
   const exitCode = dockerTasks(dummyShellJs, ["foo"]);
   expect(exitCode).toEqual(1);
-  expect(dummyShellJs.commands).toContain("echo('ERROR: Unknown option \'foo\'.')");
+  expect(dummyShellJs.echoList).toContain("ERROR: Unknown option \'foo\'.");
 });
 
 test("when we pass no command we get an error message and a non-zero exit code", () => {
   const exitCode = dockerTasks(dummyShellJs, []);
   expect(exitCode).toEqual(1);
-  expect(dummyShellJs.commands).toContain("echo('ERROR: No option chosen.')");
+  expect(dummyShellJs.echoList).toContain("ERROR: No option chosen.");
 });
--- dummyShellJs.js
+++ dummyShellJs.js
@@ -1,23 +1,24 @@
 const dummyShellJs = {
-  commands: [],
-  echo: arg => {
-    dummyShellJs.commands.push(`echo('${arg}')`);
+  _clear: () => {
+    dummyShellJs.echoList = [];
+    dummyShellJs.cpList = [];
+    dummyShellJs.execList = [];
   },
   exit: () => {},
+  echoList: [],
+  echo: arg => {
+    dummyShellJs.echoList.push(arg);
+  },
+  cpList: [],
   cp: (arg1, arg2) => {
-    dummyShellJs.commands.push(`cp('${arg1}', '${arg2}')`);
+    dummyShellJs.cpList.push(`cp ${arg1} ${arg2}`);
     return { code: 0 };
   },
+  execList: [],
   exec: arg => {
-    dummyShellJs.commands.push(`exec('${arg}')`);
+    dummyShellJs.execList.push(arg);
     return { code: 0 };
   },
-  // clear: () => {
-  //   return { ...dummyShellJs, commands: [] };
-  //   // const x = Object.clone(dummyShellJs);
-  //   // x.commands = [];
-  //   // return x;
-  // }
 }
 
 module.exports = dummyShellJs
